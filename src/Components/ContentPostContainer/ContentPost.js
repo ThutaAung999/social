43097a76c18a2e4492200255776c39ac2cdb2ed3
ThutaAung999 <exercise999123@gmail.com>
2024-12-30 15:22:42 +0630

    completed left side

---
+++ src/Components/ContentPostContainer/ContentPost.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./contentpost.css";
+
+const ContentPost = () => {
+  return <div></div>;
+};
+
+export default ContentPost;
---
+++ src/Components/MainPostContainer/MainPost.js
@@ -0,0 +1,7 @@
+    import React from "react";
+    import "./mainPost.css";
+    const MainPost = () => {
+    return <div className="mainPostContainer">MainPost</div>;
+    };
+
+    export default MainPost;
