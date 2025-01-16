8ff73307fd3c2d0fb0b797b6ed73d740a8eab740
ThutaAung999 <exercise999123@gmail.com>
2025-01-16 18:14:12 +0630

    roughtly completed login page UI

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,4 +1,41 @@
+import { Link } from 'react-router-dom';
+import './login.css';
+
 const login = () => {
-   return <div>Login</div>;
+   return (
+      <div className="mainConatinerForSignup">
+         <div className="subMainContainer">
+            <div style={{ flex: 1, marginLeft: 150, marginBottom: '170px' }}>
+               <p className="logoText">
+                  Soc<span className="part">ial</span>
+               </p>
+               <p className="introText">
+                  Connect with your
+                  <span className="part"> family and friends</span>{' '}
+               </p>
+            </div>
+            <div style={{ flex: 3 }}>
+               <p className="createAccountText">Login account</p>
+               <input type="text" placeholder="Email" className="inputText" />
+               <input
+                  type="text"
+                  placeholder="Password"
+                  className="inputText"
+               />
+               <button className="btnForSignup">Login</button>
+               <Link to={'/forgotPassword'}>
+                  <p style={{ textAlign: 'start', marginLeft: '30.6%' }}>
+                     Forgot Password?
+                  </p>
+               </Link>
+               <Link to={'/signup'}>
+                  <p style={{ textAlign: 'start', marginLeft: '30.6%' }}>
+                     Create New Account?
+                  </p>
+               </Link>
+            </div>
+         </div>
+      </div>
+   );
 };
 export default login;
